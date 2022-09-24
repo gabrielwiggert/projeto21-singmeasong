a2a7211cfa1b4ad77fda954c0b49408504cf15ba
Gabriel Wiggert <gabrielwiggert@gmail.com>
2022-09-24 03:03:38 -0300

    POST /recommendations/:id/upvote integration test implemented

--- back-end/tests/recommendations.test.ts
+++ back-end/tests/recommendations.test.ts
@@ -3,6 +3,7 @@ import supertest from "supertest";
 import app from "../src/app.js";
 import { prisma } from "../src/database.js";
 import recommendationFactory from "./factories/recommendationFactory.js";
+import { recommendationRepository } from "../src/repositories/recommendationRepository.js";
 
 beforeEach(async () => {
   await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
@@ -14,6 +15,11 @@ afterAll(async () => {
   await prisma.$disconnect();
 });
 
+async function insertRecommendation() {
+  const recommendation = await recommendationFactory();
+  await agent.post('/recommendations').send(recommendation);
+}
+
 describe('Tests POST /recommendations', () => {
   it('Should return status code 201, if a correctly formatted recommendation is created', async () => {
     const recommendation = await recommendationFactory();
@@ -21,5 +27,18 @@ describe('Tests POST /recommendations', () => {
     const result = await agent.post('/recommendations').send(recommendation);
 
     expect(result.status).toBe(201);
+  });
+});
+
+describe('Tests POST /recommendations/:id/upvote', () => {
+  it('Should return status code 200, if an upvote request on the given id is successful', async () => {
+    await insertRecommendation();
+
+    const getId = await recommendationRepository.findAll();
+    const id = getId[0].id;
+
+    const result = await agent.post(`/recommendations/${id}/upvote`);
+
+    expect(result.status).toBe(200);
   });
 });
