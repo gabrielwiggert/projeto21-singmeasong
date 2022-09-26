0560dca76c260834b6b86b97db2c1497bbfd4cc0
Gabriel Wiggert <gabrielwiggert@gmail.com>
2022-09-26 19:57:10 -0300

    Cypress tests implemented

--- front-end/cypress/e2e/usage.cy.js
+++ front-end/cypress/e2e/usage.cy.js
@@ -15,3 +15,59 @@ describe('Tests routes', () => {
     cy.url().should('equal', 'http://localhost:3000/random');
   });
 });
+
+describe('Tests submitting a recommendation + further actions', () => {
+  it('Tests submitting a recommendation', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('#Name').type("Test");
+    cy.get('#https').type("https://www.youtube.com/watch?v=75Mw8r5gW8E");
+
+    cy.get('.btn').click();
+  });
+
+  it('Tests submitting a recommendation and clicking on the video', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('#Name').type("Test");
+    cy.get('#https').type("https://www.youtube.com/watch?v=75Mw8r5gW8E");
+
+    cy.intercept('POST', 'http://localhost:5000/recommendations').as('post');
+
+    cy.get('.btn').click();
+
+    cy.wait('@post');
+
+    cy.get('[id=video]').click();
+  });
+
+  it('Tests submitting a recommendation and upvoting it', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('#Name').type("Test");
+    cy.get('#https').type("https://www.youtube.com/watch?v=75Mw8r5gW8E");
+
+    cy.intercept('POST', 'http://localhost:5000/recommendations').as('post');
+
+    cy.get('.btn').click();
+
+    cy.wait('@post');
+
+    cy.get('[id=up]').click();
+  });
+
+  it('Tests submitting a recommendation and downvoting it', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('#Name').type("Test");
+    cy.get('#https').type("https://www.youtube.com/watch?v=75Mw8r5gW8E");
+
+    cy.intercept('POST', 'http://localhost:5000/recommendations').as('post');
+
+    cy.get('.btn').click();
+
+    cy.wait('@post');
+
+    cy.get('[id=down]').click();
+  });
+});
--- front-end/src/components/Recommendation/Recommendation.js
+++ front-end/src/components/Recommendation/Recommendation.js
@@ -37,11 +37,11 @@ export default function Recommendation({ name, youtubeLink, score, id, onUpvote 
   return (
     <Container>
       <Row>{name}</Row>
-      <ReactPlayer url={youtubeLink} width="100%" height="100%" />
+      <ReactPlayer url={youtubeLink} id="video" width="100%" height="100%" />
       <Row>
-        <GoArrowUp size="24px" onClick={handleUpvote} />
+        <GoArrowUp size="24px" id="up" onClick={handleUpvote} />
         {score}
-        <GoArrowDown size="24px" onClick={handleDownvote} />
+        <GoArrowDown size="24px" id="down" onClick={handleDownvote} />
       </Row>
     </Container>
   );
